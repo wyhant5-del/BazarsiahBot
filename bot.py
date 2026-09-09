8c4683721cc7716cbb4d80ec04b542bc0b5d5a05
wyhant5-del <wyhant5@gmail.com>
2026-09-09 21:36:26 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiohttp import web  # اضافه شده برای حل مشکل پورت Render
 
-BOT_TOKEN = "8095497755:AAGbvTc4bjFBdjvpbgskMSOgboxdQtSNvGU"
+BOT_TOKEN = "8895497755:AAHB0vu6b-fyamZGlfbuvnUv1qyYdYsxalA"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()

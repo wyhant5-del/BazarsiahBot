d5c2194c43fd6ec2a8e1cb10904a55d2ac47d0bd
wyhant5-del <wyhant5@gmail.com>
2026-09-09 21:58:02 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiohttp import web
 
-BOT_TOKEN = "8095497755:AAGbvTc4bjFBdjvpbgskMSOgboxdQtSNvGU"
+BOT_TOKEN = "8895497755:AAHB0vu6b-fyamZGlfbuvnUv1qyYdYsxalA"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
